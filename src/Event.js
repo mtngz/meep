841ca0a3d9abc259c7da3ac233ecefbe7fd6a108
root <root@DESKTOP-VUKLK3J.localdomain>
2021-01-06 15:14:11 +0100

    testing feature 2

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    showDetails: false,
+  };
+
+  handleShowDetails = () => {
+    if (this.state.showDetails === false) {
+      this.setState({ showDetails: true });
+    } else {
+      this.setState({ showDetails: false });
+    }
+  };
+
   render() {
-    return <div></div>;
+    const showDetails = this.state.showDetails;
+    const { event } = this.props;
+
+    return (
+      <div className="event">
+        <div className="event-overview">
+          <h2 className="summary">{event.summary}</h2>
+          <p className="start-time">{event.start.dateTime}</p>
+          <p className="location">{event.location}</p>
+          {!showDetails && (
+            <button className="details-btn" onClick={this.handleShowDetails}>
+              Show Details
+            </button>
+          )}
+          {showDetails && (
+            <button className="details-btn" onClick={this.handleShowDetails}>
+              Hide Details
+            </button>
+          )}
+        </div>
+        {showDetails && (
+          <div className="event-details">
+            <p className="details-header">About this Event:</p>
+            <p className="details-link">See details on Google Calendar</p>
+            <p className="description">{event.description}</p>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 

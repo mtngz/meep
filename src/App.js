7776f49748d0822402e9da358035dd8177367483
mtngz <70101199+mtngz@users.noreply.github.com>
2021-01-19 14:10:04 +0100

    add responsive scatter chart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import CitySearch from "./CitySearch";
 import { getEvents, extractLocations } from "./api";
 import EventsNumber from "./EventsNumber";
 import { OfflineAlert } from "./Alert";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 class App extends Component {
   state = {
@@ -90,6 +99,28 @@ class App extends Component {
           eventsNumber={this.state.numberOfEvents}
           updateEvents={this.updateEvents}
         />
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              type="number"
+              dataKey="number"
+              name="number of events"
+              allowDecimals={false}
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+
         <EventList events={this.state.events} />
       </div>
     );

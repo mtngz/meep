55029d23cccf4009a27cf122cedc45016a3bda12
mtngz <70101199+mtngz@users.noreply.github.com>
2021-01-20 17:11:40 +0100

    update split location

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
     const data = locations.map((location) => {
       const number = events.filter((event) => event.location === location)
         .length;
-      const city = location.split(" ").shift();
+      const city = location.split(",").shift();
       return { city, number };
     });
     return data;
